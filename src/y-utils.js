ace352a257b360eb26966cf06581a9b5aadc90e2
Kevin Jahns <kevin.jahns@protonmail.com>
2026-06-10 16:42:33 +0200

    env: use-y-native=true|1 enables the feature, disabled otherwise

--- src/y-utils.js
+++ src/y-utils.js
@@ -3,7 +3,7 @@ import * as Y from '@y/y'
 import { applyUpdates } from '@y-crdt/yn'
 import { logger } from './logger.js'
 
-const useYNative = env.hasConf('use-y-native')
+const useYNative = ['true', '1'].includes(env.getConf('use-y-native') ?? '')
 
 if (useYNative) {
   logger.warn('using experimental y-native')
